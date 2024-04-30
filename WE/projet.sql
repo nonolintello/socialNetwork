19c1af5ad7c91fbcc231e7b5443ea5fa5d4c0916
nonolintello <noe.sengel@icloud.com>
2024-04-30 17:09:07 +0200

    post, edit post,supr post

--- WE/projet.sql
+++ WE/projet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : lun. 22 avr. 2024 à 19:14
+-- Généré le : mar. 30 avr. 2024 à 17:07
 -- Version du serveur : 10.4.21-MariaDB
 -- Version de PHP : 8.1.6
 
@@ -79,12 +79,29 @@ CREATE TABLE `notification` (
 CREATE TABLE `post` (
   `texte` varchar(8000) NOT NULL,
   `id` int(10) UNSIGNED NOT NULL,
+  `titre` varchar(30) NOT NULL,
   `url_image` varchar(50) DEFAULT NULL,
   `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `id_owner` int(10) UNSIGNED NOT NULL,
-  `id_parent` int(10) UNSIGNED NOT NULL,
+  `id_parent` int(10) UNSIGNED DEFAULT NULL,
   `sensible` tinyint(4) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `post`
+--
+
+INSERT INTO `post` (`texte`, `id`, `titre`, `url_image`, `date`, `id_owner`, `id_parent`, `sensible`) VALUES
+('Ceci est un exemple de texte pour le post', 3, '', '/images/post/p1', '2024-04-23 07:54:55', 8, NULL, 0),
+('Ceci est un exemple de texte pour le post', 5, '', 'https://example.com/image.jpg', '2024-04-23 07:49:07', 8, NULL, 0),
+('testc ontent', 9, 'test', NULL, '2024-04-30 11:30:53', 8, NULL, 0),
+('a', 15, 'a', NULL, '2024-04-30 11:42:38', 8, NULL, 0),
+('superTapez votre texte ici...', 16, 'test', NULL, '2024-04-30 13:08:59', 8, NULL, 0),
+('superTapez votre texte ici...', 17, 'test', NULL, '2024-04-30 13:09:30', 8, NULL, 0),
+('superTapez votre texte ici...', 18, 'test', NULL, '2024-04-30 13:10:05', 8, NULL, 0),
+('Tapez votre texte ici...', 19, 'ex', '', '2024-04-30 14:21:22', 8, NULL, 0),
+('Tapez votre texte ici...', 20, 'et', NULL, '2024-04-30 14:19:51', 8, NULL, 0),
+('Tapez votre texte ici...', 21, 'a', NULL, '2024-04-30 14:19:57', 8, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -117,7 +134,12 @@ INSERT INTO `user` (`id`, `admin`, `nom`, `prenom`, `email`, `mdp`, `url_avatar`
 (2, 0, 'Alice', 'Martin', 'alice.martin@example.com', 'motdepasse', '', 'Bonjour, je suis Alice.', NULL, '1995-03-10', '789 Avenue des Roses', 59000, 'Lille'),
 (3, 0, 'Thomas', 'Lefevre', 'thomas.lefevre@example.com', 'password', '', 'Salut, je m\'appelle Thomas.', NULL, '1992-07-25', '321 Rue des Champs', 75000, 'Paris'),
 (4, 0, 'Emma', 'Dubois', 'emma.dubois@example.com', '123456', '', 'Bonjour, je suis Emma.', NULL, '1998-11-08', '456 Boulevard du Soleil', 13000, 'Marseille'),
-(8, 0, 'a', 'a', 'a@a.com', '$2y$10$YO46xgK8Qm.NP3W/9kcTzOKhdzCKKO00id0Q/QX21elyFW8fREJc2', NULL, '', NULL, '2024-04-22', 'a', 0, 'a');
+(8, 0, 'a', 'a', 'a@a.com', '$2y$10$YO46xgK8Qm.NP3W/9kcTzOKhdzCKKO00id0Q/QX21elyFW8fREJc2', NULL, '', NULL, '2024-04-22', 'a', 0, 'a'),
+(9, 0, 'b', 'b', 'b@b.com', '$2y$10$.WI8LIiJ9BWxI62LEwBqYuS2GVCXruSLT5AJDBvrv3LPUKOALKSMm', NULL, '', NULL, '2024-04-02', 'b', 0, 'b'),
+(10, 0, 'c', 'c', 'c@c.com', '$2y$10$QEMzEbOCMYEpbSgV8TJza.IJlibdVfSbgQGuM5tEq.qtXHd69JBHm', NULL, '', NULL, '2024-04-30', 'c', 0, 'c'),
+(11, 0, 'a', 'a', 'aa@a.com', '$2y$10$cgNj00yXUnrwNwlAkXfwEOPu0AUotQHXrRzcdgsBPme4R8Aczl9.W', NULL, '', NULL, '2024-04-01', 'a', 0, 'a'),
+(12, 0, 'a', 'a', 'aa@a.com', '$2y$10$xxJiEUI3v91zkaYwnRhzCeLMT7JSZ4YBgcaak57SuwIYJ.uM4Fn1q', NULL, '', NULL, '2024-04-01', 'a', 0, 'a'),
+(13, 0, 'a', 'a', 'aa@a.com', '$2y$10$WraGykM.R.nFceRyipyXyeI91dKLBfOScBC2j8sC458br.b5CZF1a', NULL, '', NULL, '2024-04-01', 'a', 0, 'a');
 
 --
 -- Index pour les tables déchargées
@@ -174,13 +196,13 @@ ALTER TABLE `notification`
 -- AUTO_INCREMENT pour la table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(12) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(12) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Contraintes pour les tables déchargées
