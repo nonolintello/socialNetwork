5c166c7352769e556513486489e29da2a018d2d9
nonolintello <noe.sengel@icloud.com>
2024-05-05 16:46:43 +0200

    redirect to timeline when on accueil

--- WE3/ini.php
+++ WE3/ini.php
@@ -1,14 +1,11 @@
-<?php 
+<?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-define('__ROOT__',dirname(__FILE__) ); 
-define('DIR',"/WE");
+define('__ROOT__', dirname(__FILE__));
+define('DIR', "/WE3");
 
 session_start();
-require(__ROOT__."/classes/dbConn.php");
+require(__ROOT__ . "/classes/dbConn.php");
 
 $dbConn = new dbConn();
-
-?>
-
