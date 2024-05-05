48391a074ed2ff54c183fa7b617f7fc976e5a35d
taigaran <117933795+taigaran@users.noreply.github.com>
2024-05-05 16:15:21 +0200

    Delete WE2 directory

--- WE2/codePart/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer">
-    © 2024 MonSite. Tous droits réservés. 
-    <a href="#" class="text-light">Contactez-nous</a> | 
-    <a href="#" class="text-light">Conditions d'utilisation</a>
-</footer>
